5cb79384a42cb4cd68eb666053a322acca7423b2
pagewong <pageone99@hotmail.com>
2022-08-24 23:33:53 +0800

    采用dropzone前端插件

--- fflask/views.py
+++ fflask/views.py
@@ -6,30 +6,33 @@ from werkzeug.utils import secure_filename
 home = Blueprint('views', __name__)
 
 
-@home.route('', methods=['GET', 'POST'])
+@home.route('', methods=['GET'])
 def index():
+    search_path = request.args.get("path", None)
+    data_content = list_path(search_path)
+    data_head = ['文件名', '文件类型', '文件大小', '最后更新日期']
+    data = {
+        'data_head': data_head,
+        'data_content': data_content
+    }
+    return render_template('index.html', data=data)
+
+
+@home.route('/upload', methods=['POST'])
+def upload():
+    # Remember the paramName was set to 'file', we can use that here to grab it
+    file = request.files['file']
     media_root_path = current_app.config.get("MEDIA_ROOT")
-    if request.method == 'POST':
-        try:
-            files = request.files.getlist('upload_file')
-        except Exception as e:
-            resp = make_response(f"接收错误:{e}", 400)
-            return resp
-        if not files:
-            resp = make_response("没有提交任何文件", 400)
-            return resp
-        for file in files:
-            if not file.filename:
-                continue
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(media_root_path, filename))
-        return "上传成功"
-    else:
-        search_path = request.args.get("path", None)
-        data_content = list_path(search_path)
-        data_head = ['文件名', '文件类型', '文件大小', '最后更新日期']
-        data = {
-            'data_head': data_head,
-            'data_content': data_content
-        }
-        return render_template('index.html', data=data)
+    
+    # secure_filename makes sure the filename isn't unsafe to save
+    filename = secure_filename(file.filename)
+    save_path = os.path.join(media_root_path, filename)
+    
+    # We need to append to the file, and write as bytes
+    with open(save_path, 'ab') as f:
+        # Goto the offset, aka after the chunks we already wrote
+        f.seek(int(request.form['dzchunkbyteoffset']))
+        f.write(file.stream.read())
+    
+    # Giving it a 200 means it knows everything is ok
+    return make_response(('Uploaded Chunk', 200))
